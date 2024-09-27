c7bf1dc587009fb1962ac38b22212b53de38202a
ankitsinghwakefit <ankitsinghwakefit@gmail.com>
2024-09-27 11:05:18 +0530

    in memory role based authentication added

--- src/main/java/com/example/security_demo/config/SecurityConfig.java
+++ src/main/java/com/example/security_demo/config/SecurityConfig.java
@@ -3,15 +3,21 @@ package com.example.security_demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 // it's a configuration file so 
 @Configuration
 // we want spring security
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     // see there are many security filters in between thus we have to return
@@ -27,5 +33,15 @@ public class SecurityConfig {
         // means requests will be statelessso with all requests we have to pass session
         // / login creds
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // here we define our user details service
+        // we can fetch user details from database, file, etc
+        UserDetails user1 = User.withUsername("user1").password("{noop}password").roles("USER").build();
+        UserDetails admin1 = User.withUsername("admin1").password("{noop}password").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, admin1);
     }
 }
--- src/main/java/com/example/security_demo/controllers/IndexController.java
+++ src/main/java/com/example/security_demo/controllers/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,18 @@ public class IndexController {
     public String about(HttpServletRequest request) {
 
         return "This is about security" + request.getSession().getId();
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String user() {
+        return "Hello user";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello admin";
     }
 
     // either CSRF token or we can restrict the access for cross site requests in
